a7826694a9dfd1d13bdf8bf9ccac29361ee0afa2
Jiangyuan Li <jerry@Jiangyuans-MacBook-Air.local>
2024-11-22 23:50:19 -0500

    Updated Nov 22 2024

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_frozen import Freezer
+
+app = Flask(__name__)
+freezer = Freezer(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    freezer.freeze()
+
+if __name__ == '__main__':
+    app.run(debug=True)
