9b052df9b44916d8e5db604f50f3b00c87307c4b
Jerry Cyan <jerrylovesapple@icloud.com>
2025-01-24 00:32:47 -0500

    Updated Jan 24 2025

--- static/js/scroll.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", () => {
-    const title = document.getElementById("title");
-    if (window.scrollY > 100) { // Adjust scroll threshold as needed
-        title.style.fontSize = "1rem";
-    } else {
-        title.style.fontSize = "1.2rem";
-    }
-});
